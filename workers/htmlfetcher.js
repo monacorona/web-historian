773a482c0ab8703af846d5b07ced6d04e003970f
monacorona <monacoronababy@gmail.com>
2017-03-02 17:25:45 -0800

    Implement Cron

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,29 +1,24 @@
+#!/usr/env node
+
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 
-var archive = require('../helpers/archive-helpers');
+
+var archive = require('/Users/student/Codes/web-historian/helpers/archive-helpers');
 var fs = require('fs');
-var async = require('async');
+var CronJob = require('cron').CronJob;
 
-
-// // check for items in sites.txt
-// fs.readFile(archive.path.list, function(err, data) {
-//   if (err) {
-//     return console.error(err);
-//   }
-//   // form an array of sites to download
-//   var sitesArray = data.toString().split('\n');
-
-//   // iterate through these sites and download them
-//   archive.downloadUrls(sitesArray);
-  
-// });
 exports.download = archive.downloadUrls;
 
+var job = new CronJob('* * * * *', function() {
+  console.log('HELLO CRON');
+  exports.download();
+  fs.appendFile('/Users/student/Codes/web-historian/log.txt', 'cron happened\n', function(err) {
 
-  // not in our archives
-    // download array of URLs
-    
+  });
 
-    // if successful,
-      // remote from sites.txt
+});
+
+job.start();
+
+
